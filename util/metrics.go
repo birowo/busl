29c61edd65e228d56d69d96b129a70bb7c48be41
dmathieu <42@dmathieu.com>
2017-04-12 15:01:59 +0200

    measure how long storing the output takes
    We currently run this in a goroutine, meaning it can be killed within 10
    seconds if the app needs to restart.
    Measuring this will let us know if we need to better handle this, or if
    we're good with the current way for now.

--- util/metrics.go
+++ util/metrics.go
@@ -3,6 +3,8 @@ package util
 import (
 	"fmt"
 	"log"
+	"strings"
+	"time"
 )
 
 // Count parses a string into a count for logging to librato
@@ -28,4 +30,18 @@ func SampleWithData(metric string, value int64, extraData string, v ...interface
 	} else {
 		log.Printf("sample#busl.%s=%d %s", metric, value, fmt.Sprintf(extraData, v...))
 	}
+}
+
+func SMeasure(subject string, object string) string {
+	return fmt.Sprintf("measure#%s.%s=%s", prefix, subject, object)
+}
+
+func TimerStart(subject string, extras ...string) (time.Time, string, []string) {
+	log.Printf("%s.timer.start %s", subject, strings.Join(extras, " "))
+	return time.Now(), subject, extras
+}
+
+func TimerEnd(startTime time.Time, subject string, extras []string) {
+	elapsed := fmt.Sprintf("%f", time.Now().Sub(startTime).Seconds())
+	log.Printf("%s.timer.end %s %s", subject, SMeasure(subject+".elapsed.seconds", elapsed), strings.Join(extras, " "))
 }
